a8c9ad24c1772b6686a13a53fd077989c1edfbb2
Ahmed Selim Üzüm <ahmedselimuzum@ahmedselimuzum.local>
2018-08-20 17:50:31 +0300

    API İsteği Atıldı

--- app/src/main/java/com/android/ahmedselimuzum/havadurumu/anaSayfa.java
+++ app/src/main/java/com/android/ahmedselimuzum/havadurumu/anaSayfa.java
@@ -2,6 +2,15 @@ package com.android.ahmedselimuzum.havadurumu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class anaSayfa extends AppCompatActivity {
 
@@ -9,6 +18,23 @@ public class anaSayfa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ana_sayfa);
+
+        final String url=Sabitler.TEMEL_URL+Sabitler.KOORD_URL+Sabitler.API_ANAHTARI;
+
+        final JsonObjectRequest jsonIstegi=new JsonObjectRequest( Request.Method.GET, url,null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.v("HAVA","SONUC "+response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("HAVA","HATA "+error.getLocalizedMessage());
+            }
+        });
+
+        Volley.newRequestQueue(this).add(jsonIstegi);
     }
 
 
