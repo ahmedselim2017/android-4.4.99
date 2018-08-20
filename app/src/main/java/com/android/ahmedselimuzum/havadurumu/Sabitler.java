04171650481fc50b1ca1d5cced66da16e608ac25
Ahmed Selim Üzüm <ahmedselimuzum@ahmedselimuzum.local>
2018-08-20 16:11:29 +0300

    API-KEY eklendi

---
+++ app/src/main/java/com/android/ahmedselimuzum/havadurumu/Sabitler.java
@@ -0,0 +1,7 @@
+package com.android.ahmedselimuzum.havadurumu;
+
+public class Sabitler {
+    public static String  TEMEL_URL="http://api.openweathermap.org/data/2.5/forecast";
+    public static String KOORD_URL="?lat=";
+    public static String API_ANAHTARI="BURAYA OPENWEATHERMAP SİTESİNDEN ALDIĞINIZ KEY İ YAZIN";
+}
